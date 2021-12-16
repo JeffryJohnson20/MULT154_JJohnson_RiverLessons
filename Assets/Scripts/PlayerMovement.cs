de5c36d78a225f87459a6ce0a06ef4a144f90a79
JeffryJohnson20 <jeffryjohnson20@gmail.com>
2021-12-16 02:40:24 -0800

    Unity Multiplayer Lesson Finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Profiling;
+using UnityEngine.Networking;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     private Vector3 direction = Vector3.zero;
     private Rigidbody rbPlayer;
     public float speed = 10.0f;
-    public GameObject spawnPoint = null;
-    private Dictionary<Item.VegetableType, int> ItemInventory = new Dictionary<Item.VegetableType, int>();
+    public GameObject[] spawnPoints = null;
+
     // Start is called before the first frame update
     void Start()
     {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         rbPlayer = GetComponent<Rigidbody>();
-
-        foreach(Item.VegetableType item in System.Enum.GetValues(typeof(Item.VegetableType)))
-        {
-            ItemInventory.Add(item, 0);
-        }
+        spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        
     }
 
-    private void AddToInventory(Item item)
-    {
-        ItemInventory[item.typeOfVeggie]++;
-    }
-
-    private void PrintInventory()
-    {
-        string output = "";
-        foreach(KeyValuePair<Item.VegetableType, int> kvp in ItemInventory)
-        {
-            output += string.Format("{0}: {1} ", kvp.Key, kvp.Value);
-        }
-        Debug.Log(output);
-    }
+   
     private void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         float horMove = Input.GetAxis("Horizontal");
         float verMove = Input.GetAxis("Vertical");
 
@@ -45,6 +41,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         rbPlayer.AddForce(direction * speed, ForceMode.Force);
         
         if(transform.position.z > 40)
@@ -59,22 +60,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void Respawn()
     {
-        rbPlayer.MovePosition(spawnPoint.transform.position); 
+        int index = 0;
+        while(Physics.CheckBox(spawnPoints[index].transform.position, new Vector3(1.5f, 1.5f, 1.5f)))
+        {
+            index++;
+        }
+        rbPlayer.MovePosition(spawnPoints[index].transform.position); 
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Item"))
-        {
-            Item item = other.gameObject.GetComponent<Item>();
-            AddToInventory(item);
-            PrintInventory();
-        }
-    }
+    
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.CompareTag("Hazard"))
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (other.gameObject.CompareTag("Hazard"))
         {
             Respawn();
         }
